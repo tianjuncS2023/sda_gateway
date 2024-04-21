3b21be26f110868d59ca636a23f9b8ce89a5b400
tianjunc <tianjunc@andrew.cmu.edu>
2024-04-20 20:20:29 -0700

    feat: payment gateway

---
+++ src/main/java/com/star/sdagateway/SdaGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.star.sdagateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdaGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdaGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/star/sdagateway/SdaGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.star.sdagateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdaGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
